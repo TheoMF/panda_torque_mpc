0ff26b1d0699f7443f734263719a2fd56f8a70a9
Théo Martinez <tmartinezf@laas.fr>
2024-05-27 09:40:29 +0200

    retrieve correct time, change publish rate

--- include/panda_torque_mpc/crocoddyl_reaching.h
+++ include/panda_torque_mpc/crocoddyl_reaching.h
@@ -95,13 +95,16 @@ namespace panda_torque_mpc
         void set_ee_ref_placement(double time, bool is_active=true, double uniform_weight_scaling=1.0);
 
         void set_posture_ref(Eigen::VectorXd x0);
+
+        void print_ref_for_node_idx(size_t node_idx);
+
         boost::shared_ptr<crocoddyl::SolverFDDP> get_solver(){
             return ocp_;
         }
 
-        // boost::shared_ptr<crocoddyl::SolverFDDP> ocp_;
-        // boost::shared_ptr<mim_solvers::SolverSQP> ocp_;
         boost::shared_ptr<crocoddyl::SolverFDDP> ocp_;
+        // boost::shared_ptr<mim_solvers::SolverSQP> ocp_;
+        
 
         TicTac simulation_time;
         CrocoddylConfig config_;
@@ -122,7 +125,7 @@ namespace panda_torque_mpc
 
 
     bool valid_pbe();
-    bool solve(std::vector<Eigen::Matrix<double, -1, 1>> xs_init, std::vector<Eigen::Matrix<double, -1, 1>> us_init);
+    bool solve(std::vector<Eigen::Matrix<double, -1, 1>> xs_init, std::vector<Eigen::Matrix<double, -1, 1>> us_init, double nb_iter_max=1.0);
     Vector7d get_tau_ff() const;
     Eigen::MatrixXd get_ricatti_mat() const;
 
